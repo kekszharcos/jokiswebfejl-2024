9f1206aa399a89b0c7c00fa57cb4271131fea95c
kekszharcos <b.enedek670@gmail.com>
2025-03-22 11:03:10 +0100

    new env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@ export const environment = {
     appId: "1:395342193033:web:f719a85cb2a660d3fc4158",
     storageBucket: "jokiswebfejl-2024.appspot.com",
     apiKey: "AIzaSyDPrACOkNe68D7eTO-QkOq8z--4Bzgxg-4",
-    authDomain: "jokiswebfejl-2024.firebaseapp.com",
+    authDomain: "process.env.FIREBASE_API_KEY",
     messagingSenderId: "395342193033",
     measurementId: "G-12MMW3MTKL"
   }
