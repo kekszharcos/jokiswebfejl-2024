b8fca50b2f8688139c71fd27ef6280c1b6c0586c
kekszharcos <b.enedek670@gmail.com>
2025-06-08 20:26:46 +0200

    flexlayout switched to tailwind because it is not supported anymore also updated the dependencies i will start the refactoring now...

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts,scss}", // Scan all Angular component files and styles
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
