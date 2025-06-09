c12c99c00d779bf890a14afad2074aded9330355
kekszharcos <b.enedek670@gmail.com>
2025-06-09 21:18:12 +0200

    Add environment files for GitHub Actions deploy

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCyyU7SY0joW1oAL8s4tRIwB3VGXNVX-fg",
+    authDomain: "YOUR_AUTH_DOMAIN",
+    projectId: "jokiswebfejl-2024",
+    storageBucket: "YOUR_STORAGE_BUCKET",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    appId: "1:395342193033:web:7f25d442c7949d06fc4158"
+    // ...any other Firebase config values
+  }
+};
