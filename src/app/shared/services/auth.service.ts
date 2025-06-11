002186dfd5365e43d06aa315c86b0293cc7fd92b
kekszharcos <b.enedek670@gmail.com>
2025-06-12 00:19:02 +0200

    messages tab gonna be refactored as groups

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -2,10 +2,11 @@ import { Injectable } from '@angular/core';
 import { Auth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, User, browserLocalPersistence, setPersistence, authState, signInWithPopup, GoogleAuthProvider, FacebookAuthProvider, OAuthProvider } from '@angular/fire/auth';
 import { from, Observable } from 'rxjs';
 import { getAuth, updateProfile } from "firebase/auth";
+import { UserService } from './user.service';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
-  constructor(public auth: Auth) {
+  constructor(public auth: Auth, private userService: UserService) {
     setPersistence(this.auth, browserLocalPersistence); // or browserSessionPersistence
   }
 
@@ -25,7 +26,9 @@ export class AuthService {
     return this.auth.signOut();
   }
 
-  loginWithGoogle() {
-    return signInWithPopup(this.auth, new GoogleAuthProvider());
+  async loginWithGoogle() {
+    await signInWithPopup(this.auth, new GoogleAuthProvider()).then((result) => {
+      return this.userService.create(result.user.email!, result.user.uid, result.user.displayName!) 
+    });
   }
 }
